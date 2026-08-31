c59fbb1d592533f1b21e10b83457ceb4b54f92cf
platocres <49848326+platocres@users.noreply.github.com>
2026-08-31 15:08:50 +0000

    Advance current release authority to v9.3

--- data/current-release.js
+++ data/current-release.js
@@ -1,8 +1,8 @@
 'use strict';
 (function(root){
 const release=Object.freeze({
- version:'9.2.0',
- label:'v9.2',
+ version:'9.3.0',
+ label:'v9.3',
  phase:'product-hardening',
  phaseLabel:'Product Hardening',
  orangeBaseline:'v8.8'
